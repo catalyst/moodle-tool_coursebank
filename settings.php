2393f805ba08417c051744ed7f54def6dd06b5ff
Adam Riddell <adamr@catalyst-au.net>
2015-03-11 18:10:50 +1100

    WR#238561: Add config options for web services
            settings.php

--- settings.php
+++ settings.php
@@ -19,16 +19,59 @@
  *
  * @package    tool_coursestore
  * @author     Ghada El-Zoghbi <ghada@catalyst-au.net>
- * @copyright  2015 Catalys IT
+ * @author     Adam Riddell <adamr@catalyst-au.net>
+ * @copyright  2015 Catalyst IT
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
 
 
 defined('MOODLE_INTERNAL') || die();
 
-if ($hassiteconfig) {
-	if (is_siteadmin()) {
-	    $ADMIN->add('backups', new admin_externalpage('toolcoursestore',
-	        get_string('sendcoursebackups', 'tool_coursestore'), "$CFG->wwwroot/$CFG->admin/tool/coursestore/index.php"));
-	}
+require_once($CFG->libdir.'/adminlib.php');
+
+if ($ADMIN->fulltree) {
+    $settings = new admin_settingpage('coursestore_settings',
+            get_string('pluginname', 'tool_coursestore')
+    );
+    $settings->add(new admin_setting_heading('coursestore_header',
+            get_string('settings_header', 'tool_coursestore'),
+            '')
+    );
+    $enableoptions = array(
+        0 => get_string('settings_disablestring', 'tool_coursestore'),
+        1 => get_string('settings_enablestring', 'tool_coursestore')
+    );
+    $enable = new admin_setting_configselect('tool_coursestore/enable',
+            ' '.get_string('settings_enable', 'tool_coursestore'),
+            ' '.get_string('settings_enable_desc', 'tool_coursestore'),
+            0,
+            $enableoptions
+    );
+    $settings->add($enable);
+    $settings->add(new admin_setting_configtext('tool_coursestore/url',
+            get_string('settings_url', 'tool_coursestore'),
+            get_string('settings_url_desc', 'tool_coursestore'),
+            '',
+            PARAM_URL)
+    );
+    $settings->add(new admin_setting_configtext('tool_coursestore/chunksize',
+            get_string('settings_chunksize', 'tool_coursestore'),
+            get_string('settings_chunksize_desc', 'tool_coursestore'),
+            100,
+            PARAM_INT)
+    );
+    $settings->add(new admin_setting_configtext('tool_coursestore/timeout',
+            get_string('settings_timeout', 'tool_coursestore'),
+            get_string('settings_timeout_desc', 'tool_coursestore'),
+            10,
+            PARAM_INT)
+    );
+    $settings->add(new admin_setting_configtext('tool_coursestore/conntimeout',
+            get_string('settings_conntimeout', 'tool_coursestore'),
+            get_string('settings_conntimeout_desc', 'tool_coursestore'),
+            5,
+            PARAM_INT)
+    );
+
+    $ADMIN->add('tools', $settings);
 }
