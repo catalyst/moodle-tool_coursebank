f872cd434417bfd8b5f34a2d09948c17ac958e0e
Tim Price <timprice@catalyst-au.net>
2015-03-19 09:20:52 +1100

    WR#238561 - fixing bug in connection check javascript so that only one status can show at a time

--- javascript/coursestore.js
+++ javascript/coursestore.js
@@ -1,18 +1,25 @@
 jQuery(document).ready(function() {
-	jQuery('.singlebutton').click( submit_check );
+	    jQuery('.singlebutton').click( submit_check );
     }
 );
+
 function submit_check( event ) {
     event.preventDefault();
     var xhr = new XMLHttpRequest();
     var wwwroot = jQuery('.wwwroot').val();
-    xhr.onreadystatechange = function() { //TODO: error handling
+    xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            var response = xhr.responseText; //If all goes to plan then the response is the id of the new discussion
+            var response = xhr.responseText; 
             if (response == '"1"') {
-            	jQuery(".notification-success").removeClass("hide");    	
+            	jQuery(".notification-success").removeClass("hide");
+            	if (!jQuery(".notification-fail").hasClass("hide")) {
+            	    jQuery(".notification-fail").addClass("hide");
+            	}
             } else {
             	jQuery(".notification-fail").removeClass("hide");
+            	if (!jQuery(".notification-success").hasClass("hide")) {
+            	    jQuery(".notification-success").addClass("hide");
+            	}
             }
         }
     };
