8b2f403684e76b1dbafd61c1cffa737bc389c5b3
Adam Riddell <adamr@catalyst-au.net>
2015-03-23 16:38:10 +1100

    WR#238561: Move backup process into locallib
            locallib.php:
                    - Move main backup logic flow into fetch_backups
                      function in locallib.php
                    - Fix course store record insertion code to ensure that
                      fields are correctly set.

--- locallib.php
+++ locallib.php
@@ -278,6 +278,167 @@ abstract class tool_coursestore {
 
         return true;
     }
+    /**
+     * Function to fetch course backup records from the Moodle DB, add them
+     * to the course store table, and then process the files before sending
+     * via web service to the configured course bank instance.
+     *
+     * - The query consists of three parts which are combined with a UNION
+     * - The first
+     *
+     */
+    public static function fetch_backups() {
+        global $CFG, $DB;
+
+        // Get a list of the course backups.
+        $sqlcommon = "SELECT tcs.id,
+                       tcs.backupfilename,
+                       tcs.fileid,
+                       tcs.chunksize,
+                       tcs.totalchunks,
+                       tcs.chunknumber,
+                       tcs.timecreated,
+                       tcs.timecompleted,
+                       tcs.timechunksent,
+                       tcs.timechunkcompleted,
+                       tcs.chunkretries,
+                       tcs.status,
+                       tcs.isbackedup,
+                       f.id AS f_fileid,
+                       f.contenthash,
+                       f.pathnamehash,
+                       f.filename,
+                       f.userid,
+                       f.filesize,
+                       f.timecreated AS filetimecreated,
+                       f.timemodified AS filetimemodified,
+                       cr.id AS courseid,
+                       cr.fullname AS coursefullname,
+                       cr.shortname AS courseshortname,
+                       cr.category,
+                       cr.startdate AS coursestartdate,
+                       cc.id as categoryid,
+                       cc.name as categoryname
+                FROM {files} f
+                INNER JOIN {context} ct on f.contextid = ct.id
+                INNER JOIN {course} cr on ct.instanceid = cr.id
+                INNER JOIN {course_categories} cc on cr.category = cc.id";
+
+        $sqlselect = "SELECT tcs.id,
+                       tcs.backupfilename,
+                       tcs.fileid,
+                       tcs.chunksize,
+                       tcs.totalchunks,
+                       tcs.chunknumber,
+                       tcs.timecreated,
+                       tcs.timecompleted,
+                       tcs.timechunksent,
+                       tcs.timechunkcompleted,
+                       tcs.chunkretries,
+                       tcs.status,
+                       tcs.isbackedup,
+                       f.id AS f_fileid,
+                       tcs.contenthash,
+                       tcs.pathnamehash,
+                       f.filename,
+                       tcs.userid,
+                       tcs.filesize,
+                       tcs.filetimecreated,
+                       tcs.filetimemodified,
+                       tcs.courseid,
+                       tcs.coursefullname,
+                       tcs.courseshortname,
+                       cr.category,
+                       tcs.coursestartdate,
+                       tcs.categoryid,
+                       tcs.categoryname
+                FROM {files} f
+                INNER JOIN {context} ct on f.contextid = ct.id
+                INNER JOIN {course} cr on ct.instanceid = cr.id
+                INNER JOIN {course_categories} cc on cr.category = cc.id";
+
+        $sql = $sqlcommon . "
+                LEFT JOIN {tool_coursestore} tcs on tcs.fileid = f.id
+                WHERE tcs.id IS NULL
+                AND ct.contextlevel = :contextcourse1
+                AND   f.mimetype IN ('application/vnd.moodle.backup', 'application/x-gzip')
+                UNION
+                " . $sqlselect . "
+                INNER JOIN {tool_coursestore} tcs on tcs.fileid = f.id
+                WHERE ct.contextlevel = :contextcourse2
+                AND   f.mimetype IN ('application/vnd.moodle.backup', 'application/x-gzip')
+                AND   (tcs.status IN (:statusnotstarted, :statusinprogress)
+                      OR (tcs.status = :statuserror))
+                UNION
+                " . $sqlselect . "
+                RIGHT JOIN {tool_coursestore} tcs on tcs.fileid = f.id
+                WHERE f.id IS NULL
+                AND tcs.isbackedup = 1";
+        $params = array('statusnotstarted' => tool_coursestore::STATUS_NOTSTARTED,
+                        'statuserror' => tool_coursestore::STATUS_ERROR,
+                        'statusinprogress' => tool_coursestore::STATUS_INPROGRESS,
+                        'contextcourse1' => CONTEXT_COURSE,
+                        'contextcourse2' => CONTEXT_COURSE
+                        );
+        $rs = $DB->get_recordset_sql($sql, $params);
+
+        $insertfields = array('filesize', 'filetimecreated',
+                'filetimemodified', 'courseid', 'contenthash',
+                'pathnamehash', 'userid', 'coursefullname',
+                'courseshortname', 'coursestartdate', 'categoryid',
+                'categoryname'
+        );
+        foreach ($rs as $coursebackup) {
+            if (!isset($coursebackup->status)) {
+                // The record hasn't been input in the course restore table yet.
+                $cs = new stdClass();
+                $cs->backupfilename = $coursebackup->filename;
+                $cs->fileid = $coursebackup->f_fileid;
+                $cs->chunksize = tool_coursestore::get_config_chunk_size();
+                $cs->totalchunks = tool_coursestore::calculate_total_chunks($cs->chunksize, $coursebackup->filesize);
+                $cs->chunknumber = 0;
+                $cs->status = tool_coursestore::STATUS_NOTSTARTED;
+                $cs->isbackedup = 0; // No copy has been created yet.
+                foreach($insertfields as $field) {
+                    $cs->$field = $coursebackup->$field;
+                }
+                $backupid = $DB->insert_record('tool_coursestore', $cs);
+
+                $coursebackup->id = $backupid;
+                $coursebackup->backupfilename = $cs->backupfilename;
+                $coursebackup->fileid = $cs->fileid;
+                $coursebackup->chunksize = $cs->chunksize;
+                $coursebackup->totalchunks = $cs->totalchunks;
+                $coursebackup->chunknumber = $cs->chunknumber;
+                $coursebackup->timecreated = 0;
+                $coursebackup->timecompleted = 0;
+                $coursebackup->timechunksent = 0;
+                $coursebackup->timechunkcompleted = 0;
+                $coursebackup->chunkretries = 0;
+                $coursebackup->status = $cs->status;
+            }
+            $result = tool_coursestore::send_backup($coursebackup);
+            if ($result) {
+                $delete = tool_coursestore::delete_backup($coursebackup);
+                if (!$delete) {
+                    $delfail = get_string(
+                            'deletefailed',
+                            'tool_coursestore',
+                            $coursebackup->filename
+                    );
+                    mtrace($delfail . "\n");
+                }
+            } else {
+                $bufail = get_string(
+                        'backupfailed',
+                        'tool_coursestore',
+                        $coursebackup->filename
+                );
+                mtrace($bufail . "\n");
+            }
+        }
+        $rs->close();
+    }
 }
 
 /**
