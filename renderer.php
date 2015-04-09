d88f77331b2589fc99dcd77a1681b76eacb7cb87
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2015-04-09 14:38:21 +1000

    WR#238561: fixed date output bug

--- renderer.php
+++ renderer.php
@@ -97,7 +97,7 @@ class tool_coursestore_renderer extends plugin_renderer_base {
             $html .= html_writer::tag('td', $download->coursefullname);
             $html .= html_writer::tag('td', $download->backupfilename);
             $html .= html_writer::tag('td', display_size($download->filesize));
-            $html .= html_writer::tag('td', userdate($download->filetimemodified));
+            $html .= html_writer::tag('td', userdate(strtotime($download->filetimemodified)));
             // TO DO: actual link to download.
             $text = get_string('download');
             $icon = html_writer::empty_tag('img', array('src' => $this->output->pix_url('/t/download'),
