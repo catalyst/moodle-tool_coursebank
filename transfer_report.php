f5f4eb6c4ff358782eda56b34b49084e7859c004
Tim Price <timprice@catalyst-au.net>
2015-08-26 14:27:18 +1000

    WR#238561 - fixing use of MOODLE_INTERNAL flag

--- transfer_report.php
+++ transfer_report.php
@@ -23,8 +23,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
-
 require_once('../../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 require_once($CFG->dirroot.'/admin/tool/coursebank/locallib.php');
