ddd24eba764f51fd87a2f9a30943823b648e05ef
Daniel Bush <danb@catalyst-au.net>
2015-09-09 16:35:38 +1000

    WR#238561: Bumping plugin version and release.

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015090700;
+$plugin->version   = 2015090900;
 $plugin->requires  = 2012120300;
 $plugin->component = 'tool_coursebank';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "2015090100";
+$plugin->release   = "2015090900";
