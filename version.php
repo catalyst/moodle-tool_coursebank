42747e8d921c48e7b6af41d5f0a4047b0d7d9a83
Kenneth Hendricks <kennethhendricks@catalyst-au.net>
2016-06-28 10:47:05 +1000

    Bump version

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015103000;
+$plugin->version   = 2016062800;
 $plugin->requires  = 2012120300;
 $plugin->component = 'tool_coursebank';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "2015103000";
+$plugin->release   = "2016062800";
